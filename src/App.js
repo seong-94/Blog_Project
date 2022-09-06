70ac09e6fef13798ca8f6e17802d686d9b26efae
44502699 <68951572+seong-94@users.noreply.github.com>
2022-09-07 00:57:56 +0900

    Navigation menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Home from "./components/Home";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
+import CreatePost from "./components/CreatePost";
+import "./css/App.css";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/login"> login </Link>
+        <Link to="/createpost"> createpost </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createpost" element={<CreatePost />} />
       </Routes>
     </Router>
   );
